baa651ed792ae3179b544e8df99ef95590a9f777
Jose Arniel Pama <pamajosearniel@gmail.com>
2025-02-05 03:03:39 +0800

    add code for triggering picture-in-picture; prompt to select media stream, pass to video element, then play

---
+++ script.js
@@ -0,0 +1,31 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch error here
+    alert("Error in selecting media screen.");
+    console.error("error here: ", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+
+  // Reset button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
